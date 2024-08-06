f3726a770bf3c148fa83fbb5093153569c35228f
Seth Nickell <snickell@gmail.com>
2024-08-06 04:17:51 -1000

    For clarity, show loading pycall only in the thread

--- crash_puma.rb
+++ crash_puma.rb
@@ -1,23 +1,23 @@
 #!/usr/bin/env ruby
 
-# Setup our local venv (using pdm, in .venv)
-ENV['PYTHON'] = `pdm run which python`.strip
-SITEDIR = `pdm run python -c 'import site; print(site.getsitepackages()[0])'`.strip
-
-require 'pycall'
-
-# This is to setup our local venv
-site = PyCall.import_module('site')
-site.addsitedir(SITEDIR)
-
 module CrashPuma
 
   def self.do_crash
+    # Setup our local venv (using pdm, in .venv)
+    ENV['PYTHON'] = `pdm run which python`.strip
+    site_dir = `pdm run python -c 'import site; print(site.getsitepackages()[0])'`.strip
+
+    require 'pycall'
+
+    # This is to setup our local venv
+    site = PyCall.import_module('site')
+    site.addsitedir(site_dir)
+
+    # Now import the module that will crash
     input_scanners = PyCall.import_module('llm_guard.input_scanners')
 
     puts "About to crash (if running in puma)"
 
-    
     ### THIS RUNS PYTHON CODE THAT CRASHES IF RUN IN PUMA
     ### BUT WORKS IF RUN OUTSIDE PUMA.
     input_scanners.Toxicity() # SEGV if run in puma, works if run directly
@@ -29,5 +29,6 @@ module CrashPuma
 end
 
 if __FILE__ == $0
+  # Does not crash if run like `./crash_puma.rb`, try `./crash_puma.sh` instead to see a crash
   CrashPuma.do_crash
 end
