5e2a93d2f8d077651b67fb2eec2357cd8ebe3934
Seth Nickell <snickell@gmail.com>
2024-08-06 05:02:51 -1000

    Revert to original block setup

--- crash_puma.rb
+++ crash_puma.rb
@@ -1,28 +1,22 @@
 #!/usr/bin/env ruby
 
+# Setup our local venv (using pdm, in .venv)
+ENV['PYTHON'] = `pdm run which python`.strip
+site_dir = `pdm run python -c 'import site; print(site.getsitepackages()[0])'`.strip
+
+require 'pycall'
+
+# This is to setup our local venv
+site = PyCall.import_module('site')
+site.addsitedir(site_dir)
+
 module CrashPuma
 
   def self.do_crash
-    # Setup our local venv (using pdm, in .venv)
-    ENV['PYTHON'] = `pdm run which python`.strip
-    site_dir = `pdm run python -c 'import site; print(site.getsitepackages()[0])'`.strip
-
-    require 'pycall'
-
-    # This is to setup our local venv
-    site = PyCall.import_module('site')
-    site.addsitedir(site_dir)
-
-    # Now import the module that will crash
-    input_scanners = PyCall.import_module('llm_guard.input_scanners')
-
     puts "About to crash (if running in puma)"
 
-    ### THIS RUNS PYTHON CODE THAT CRASHES IF RUN IN PUMA
-    ### BUT WORKS IF RUN OUTSIDE PUMA.
+    input_scanners = PyCall.import_module('llm_guard.input_scanners')
     input_scanners.Toxicity() # SEGV if run in puma, works if run directly
-    ### END CRASH CODE
-
 
     puts "IT DID NOT CRASH"
   end
