d11de86d496c16d3b31466f3a095e34c4b3116fb
Seth Nickell <snickell@gmail.com>
2024-08-08 03:28:24 -1000

    app.rb: rubocop

--- examples/puma/app.rb
+++ examples/puma/app.rb
@@ -1,25 +1,26 @@
-require 'pycall_thread'
+require "pycall_thread"
 
 PyCallThread.init do
   # Setup our local venv (using pdm, in .venv)
-  ENV['PYTHON'] = `pdm run which python`.strip
+  ENV["PYTHON"] = `pdm run which python`.strip
   site_dir = `pdm run python -c 'import site; print(site.getsitepackages()[0])'`.strip
 
-  require 'pycall'
+  require "pycall"
 
   # This is to setup our local venv
-  site = PyCall.import_module('site')
+  site = PyCall.import_module("site")
   site.addsitedir(site_dir)
 end
 
+# Simple Puma App that demonstrates PyCallThread
 class App
-  def call(env)
+  def call(_)
     winequality = PyCallThread.run do
-      pandas = PyCall.import_module('pandas')
-      data = pandas.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv', sep: ';')
-      data.to_json()
+      pandas = PyCall.import_module("pandas")
+      data = pandas.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv", sep: ";")
+      data.to_html
     end
 
-    [200, { 'Content-Type' => 'text/html' }, [winequality]]
+    [200, { "Content-Type" => "text/html" }, [winequality]]
   end
 end
